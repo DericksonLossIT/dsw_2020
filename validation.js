2117da05bbd64b63ba7e1b774b291aa4e324dcef
DericksonLossIT <dericksonlossit@gmail.com>
2020-05-04 11:11:21 -0300

    Enviando os arquivos para o repositório remoto

---
+++ validation.js
@@ -0,0 +1,62 @@
+function validarNome(){
+    var valor = document.forms["formCadastroProduto"]["txtNomeProduto"].value;
+
+    if(valor == ""){
+        alert("O campo nome do produto não foi preenchido");
+        return false;
+    }
+}
+
+function validarPreco(){
+    var valor = document.forms["formCadastroProduto"]["txtPreco"].value;
+
+    if(valor == ""){
+        alert("O campo preço não foi preenchido!");
+        return false;
+    }else if(valor <= 0){
+        alert("O preço do produto não pode ser menor ou igual a zero!");
+        return false;
+    }
+}
+
+function validarQtd(){
+    var valor = document.forms["formCadastroProduto"]["txtQtd"].value;
+
+    if(valor == ""){
+        alert("O campo quantidade não foi preenchido!");
+        return false;
+    }
+}
+
+function validarDataValidade(){
+    var dataUser = new Date(document.forms["formCadastroProduto"]["txtDtValidade"].value).valueOf();
+    var dataValidar = new Date.now();
+    
+    alert(dataUser);
+    alert(dataValidar);
+
+    
+
+    
+
+}
+
+function validarCategoria(){
+    var valor = document.forms["formCadastroProduto"]["txtCategoria"].value;
+    
+
+    if(valor == "" || valor == "selecione"){
+        alert("Você deve selecionar uma categoria!");
+        return false;
+    }
+}
+
+function validarFormulario(){
+    var validacao = true;
+    validarNome();
+    validarPreco();
+    validarQtd();
+    validarDataValidade();
+    validarCategoria();
+    return validacao;
+}
